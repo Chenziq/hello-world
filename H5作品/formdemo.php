64035a756de2b6edc6bc6f614c84c2130c7e6413
Chenziq <48311206+Chenziq@users.noreply.github.com>
2019-06-03 12:43:16 +0800

    Add files via upload

---
+++ H5作品/formdemo.php
@@ -0,0 +1,15 @@
+﻿<?php
+	header("Content-Type:text/html;charset=utf8");
+	echo $_POST['name']."，恭喜你注册成功！"."<br>";
+	echo "您注册的邮箱是：".$_POST['email']."<br>";
+	echo "您注册的密码是：".$_POST['password']."<br>";
+	$_user=$_POST['name'];
+	$_email=$_POST['email'];
+	$_age=$_POST['password'];
+	@mysql_connect('127.0.0.1','zjwdb_6271079','Abc199607010') or die('数据库连接失败'.mysql_error());
+	mysql_select_db('zjwdb_6271079');
+	mysql_query('SET NAMES UTF8');
+	mysql_query("insert into user values('$_user','$_email','$_age');");
+	header("refresh:10;url=login.html");
+	print('<br>10秒后跳转到登录页面~~~~~');
+?>	
---
+++ H5作品/login.php
@@ -0,0 +1,16 @@
+﻿<?php
+	header("Content-Type:text/html;charset=utf8");
+	$_user=$_POST['name'];
+	$_age=$_POST['password'];
+	@mysql_connect('127.0.0.1','zjwdb_6271079','Abc199607010') or die('数据库连接失败'.mysql_error());
+	mysql_select_db('zjwdb_6271079');
+	mysql_query('SET NAMES UTF8');
+	$check_query=mysql_query("select * from user where username='$_user' and age='$_age' limit 1");
+	if(mysql_fetch_array($check_query)){
+		setcookie('username',$_user);
+		header("location:index.php");
+	}else{
+		header("refresh:5;url=login.html");
+		print('用户名或者密码有误，请重新登录！<br>5秒后跳转到登录页面。');
+	}
+?>
